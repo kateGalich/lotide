1e2e0389d1e78189423ab0e98e450b4225457d5f
Kate Galich <kate.galich@outlook.com>
2022-09-19 00:57:54 +0000

    Function compared two arrays added

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
