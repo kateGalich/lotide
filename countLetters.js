6f5a606ef521d6415e49b3afcb974a7c195e8ef7
Kate Galich <kate.galich@outlook.com>
2022-09-19 16:05:32 +0000

    added function to count letters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  let result = {};
+
+  for (let char of sentence) {
+    if (char != ' ') {
+
+      if (!result[char]) {
+        result[char] = 1;
+      } else {
+        result[char] += 1;
+      }
+    }
+  }
+  return result;
+}
+console.log(countLetters('lighthouse in the house'));
