64605edcb93e8c848b62e626966625f2c5f5bdaf
Kate Galich <kate.galich@outlook.com>
2022-09-25 21:07:46 -0400

    adding module.exports for eqArrays fn and separated test file

--- eqArrays.js
+++ eqArrays.js
@@ -8,17 +8,4 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+module.exports = eqArrays;
--- test/headTest.js
+++ test/headTest.js
@@ -2,8 +2,8 @@ const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 
-assertEqual(head([]), 5);
+assertEqual(head([]), undefined);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([3,4,2,5]), 5);
+assertEqual(head([3,4,2,5]), 3);
 assertEqual(head([5,6,7]), 5);
 
